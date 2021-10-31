8bd52d880f5d2f27814b8252ec8b9d8f25f1b5d6
ZJ <68308477+ZJTAN97@users.noreply.github.com>
2021-10-31 10:39:17 +0800

    can remove already but still buggy

--- Drag and Drop Game/appscripts/main.js
+++ Drag and Drop Game/appscripts/main.js
@@ -36,28 +36,63 @@ for(i=0; i<5; i++) {
 let cardActions = function(card) {
 
     let state = 0;
+    let allocated = false;
     card.node.addEventListener('mousedown', function(){
         console.log('down-ed')
         state = 1;
+        console.log(state)
+
     })
     card.node.addEventListener('mouseup', function(){
         console.log('up-ed')
         state = 0;
     })
     card.node.addEventListener('mousemove', function(move){
-        if (state === 1) {
-            card.attr({
-                "x" : move.offsetX - (cardWidth/2), //to ensure that the movement corresponds to the centre of rect rather than the top left corner
-                "y" : move.offsetY - (cardHeight/2), //to ensure that the movement corresponds to the centre of rect rather than the top left corner
-            })
+
+        // during mouse move you want to check if the card is allocated to a box
+        // if its not allocated, you will use `enterBox` to check which box its going to be allocated to
+        // if allocated, you use `exitBox` to remove from the box
+
+        // IF NOT ALLOCATED
+        if(!allocated) {
+            if (state === 1) {
+                card.attr({
+                    "x" : move.offsetX - (cardWidth/2), 
+                    "y" : move.offsetY - (cardHeight/2),
+                })
+                allocated = enterBox(card)
+                if(allocated) {
+                    state = 0
+                } else {
+                    state = 1
+                }
+            }
         }
-        allocated = enterBox(card)
+
+        // IF ALLOCATED
+        if(allocated) {
+            if (state === 1) {
+                card.attr({
+                    "x" : move.offsetX - (cardWidth/2), 
+                    "y" : move.offsetY - (cardHeight/2), 
+                })
+                exitBox(card)            
+            }
+        }
+
     })
 }
 
 
 let enterBox = function(card) {    
-    if (card.attrs.y <= 601 + 70 && card.attrs.y >= 601 - 70 && card.attrs.x >= 117 - 70 && card.attrs.x <= 117+ 70) {
+    // Currently you only have the condition for first box, you need to to it for all boxes
+    let TOLERANCE = 70; // how much gap you want to give before the card is ASSIGNED to the box.
+    if (
+        card.attrs.y <= 601 + TOLERANCE && 
+        card.attrs.y >= 601 - TOLERANCE && 
+        card.attrs.x >= 117 - TOLERANCE && 
+        card.attrs.x <= 117+ TOLERANCE
+    ) {
         card.attr({
             x: 117,
             y: 601.5
@@ -65,6 +100,14 @@ let enterBox = function(card) {
         boxArray[0].hide()
         return true;
     } 
+    return false;
+}
+
+let exitBox = function(card) {
+    let TOLERANCE = 10
+    if (card.attrs.y <= 601 - TOLERANCE) {
+        boxArray[0].show()
+    }
 }
 
 
@@ -72,8 +115,7 @@ for(i=0; i<5; i++) {
     // add actions to all the cards
     cardActions(cardStack[i])    
 }
-
-
+/** End of Actions Handler */
 
 
 
